8d46231c2bf81cdb665896f0e5de48172d949e27
GinajiyoungSong <jejuladygina@gmail.com>
2020-01-03 18:21:13 +0900

    0103

--- jeju.sql
+++ jeju.sql
@@ -1,348 +1,74 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.4.11-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  10.2.0.5599
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- jeju 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `jeju` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `jeju`;
-
--- 테이블 jeju.authors 구조 내보내기
-CREATE TABLE IF NOT EXISTS `authors` (
-  `name` varchar(50) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `test` int(11) DEFAULT NULL,
-  `test2` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 테이블 데이터 jeju.authors:~2 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` (`name`, `address`, `test`, `test2`) VALUES
-	('대한민국', '서울시', NULL, NULL),
-	('민국이', '대전시', NULL, NULL);
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
-
--- 테이블 jeju.goods 구조 내보내기
-CREATE TABLE IF NOT EXISTS `goods` (
-  `code` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `su` int(11) DEFAULT NULL,
-  `dan` int(11) DEFAULT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='primary key : 중복이 불가능, 외부에서 참조가 가능한 키';
-
--- 테이블 데이터 jeju.goods:~7 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `goods` DISABLE KEYS */;
-INSERT INTO `goods` (`code`, `name`, `su`, `dan`) VALUES
-	(1, '냉장고', 2, 850000),
-	(2, '세탁기', 3, 550000),
-	(3, '전자레인지', 2, 350000),
-	(4, 'HDTV', 3, 1500000),
-	(5, 'VR기기', 2, 1440000),
-	(6, 'Drone', 1, 890000),
-	(7, '고성능컴퓨터', 3, NULL);
-/*!40000 ALTER TABLE `goods` ENABLE KEYS */;
-
--- 테이블 jeju.purchase 구조 내보내기
-CREATE TABLE IF NOT EXISTS `purchase` (
-  `num` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` char(3) DEFAULT NULL,
-  `prodName` varchar(20) DEFAULT NULL,
-  `groupName` varchar(20) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL,
-  PRIMARY KEY (`num`),
-  KEY `userID` (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
--- 테이블 데이터 jeju.purchase:~0 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
-INSERT INTO `purchase` (`num`, `userID`, `prodName`, `groupName`, `price`, `amount`) VALUES
-	(1, 'KBS', '운동화', '전자', 30, 2),
-	(2, 'KBS', '노트북', '전자', 1000, 1),
-	(3, 'JYP', '모니터', '전자', 200, 1),
-	(4, 'BBK', '모니터', '전자', 200, 5),
-	(5, 'KBS', '청바지', '의류', 50, 3),
-	(6, 'BBK', '메모리', '전자', 80, 10),
-	(7, 'SSK', '책', '서적', 15, 5),
-	(8, 'EJW', '책', '서적', 15, 2),
-	(9, 'EJW', '청바지', '의류', 50, 1),
-	(10, 'BBK', '운동화', '전자', 30, 2),
-	(11, 'EJW', '책', '서적', 15, 1),
-	(12, 'BBK', '운동화', '서적', 30, 2);
-/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
-
--- 테이블 jeju.school 구조 내보내기
-CREATE TABLE IF NOT EXISTS `school` (
-  `no` int(11) NOT NULL AUTO_INCREMENT,
-  `schoolname` varchar(20) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `schoolcode` char(10) NOT NULL,
-  `studentcount` int(11) DEFAULT NULL,
-  PRIMARY KEY (`schoolcode`),
-  KEY `no` (`no`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='varchar : 가변적\r\nchar : (10) 자리로 고정\r\nkey : 중복을 불허함\r\nauto_increment : 입력하지 않고 자동으로 입력됨';
-
--- 테이블 데이터 jeju.school:~4 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `school` DISABLE KEYS */;
-INSERT INTO `school` (`no`, `schoolname`, `address`, `schoolcode`, `studentcount`) VALUES
-	(1, '충주여자고등학교', '충주시', 'CH00000001', 300),
-	(2, '신성여자고등학교', '제주시', 'IC00000001', 560),
-	(3, '종로여자고등학교', '종로구', 'JR00000001', 300),
-	(4, '제주여자고등학교', '제주시', 'SE00000001', 300);
-/*!40000 ALTER TABLE `school` ENABLE KEYS */;
-
--- 테이블 jeju.student 구조 내보내기
-CREATE TABLE IF NOT EXISTS `student` (
-  `bunho` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `kor` tinyint(4) DEFAULT NULL,
-  `mat` tinyint(4) DEFAULT NULL,
-  `eng` tinyint(4) DEFAULT NULL,
-  `total` int(11) DEFAULT NULL,
-  `average` float DEFAULT NULL,
-  `grade` char(2) DEFAULT NULL,
-  `schoolcode` char(10) DEFAULT NULL,
-  PRIMARY KEY (`bunho`),
-  KEY `schoolcode` (`schoolcode`),
-  CONSTRAINT `FK__school` FOREIGN KEY (`schoolcode`) REFERENCES `school` (`schoolcode`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
--- 테이블 데이터 jeju.student:~8 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` (`bunho`, `name`, `kor`, `mat`, `eng`, `total`, `average`, `grade`, `schoolcode`) VALUES
-	(1, '전공인', 82, 83, 92, 257, 85.67, 'B', 'SE00000001'),
-	(2, '전공이', 83, 83, 83, 249, 83, 'B', 'SE00000001'),
-	(3, '팔전공', 83, 83, 83, 249, 83, 'B', 'CH00000001'),
-	(4, '전공사', 93, 102, 102, 297, 99, 'A', 'CH00000001'),
-	(5, '고려인', 102, 102, 102, 306, 102, 'A', 'CH00000001'),
-	(6, '종로구', 102, 83, 83, 268, 89.33, 'B', 'IC00000001'),
-	(7, '김만덕', 102, 83, 73, 258, 86, 'B', 'IC00000001'),
-	(8, '김제주', 102, 102, 102, 306, 102, 'A', 'IC00000001');
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-
--- 테이블 jeju.student2 구조 내보내기
-CREATE TABLE IF NOT EXISTS `student2` (
-  `num` int(11) NOT NULL,
-  `name` varchar(12) DEFAULT NULL,
-  `sex` char(1) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `major` varchar(20) DEFAULT NULL,
-  `addr` varchar(15) DEFAULT NULL,
-  `tel` varchar(15) DEFAULT NULL,
-  `money` int(11) DEFAULT NULL,
-  PRIMARY KEY (`num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='20120001, ''고길동'', ''m'', 27, ''선박'', ''seoul'', ''010-0000-4152'', 5000\r\n원래 전공/직업은 code화해야 함! (ex. 역사 2, 선박 1, ...)';
-
--- 테이블 데이터 jeju.student2:~6 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `student2` DISABLE KEYS */;
-INSERT INTO `student2` (`num`, `name`, `sex`, `age`, `major`, `addr`, `tel`, `money`) VALUES
-	(20120001, '고길동', 'm', 27, '선박', 'seoul', '010-000-4512', 5000),
-	(20120002, '최둘리', 'm', 22, '역사', 'busan', '010-999-9999', 4500),
-	(20120003, '도우너', 'w', 15, '역사', 'daegu', '010-555-5555', 6500),
-	(20120004, '김만덕', 'w', 15, '역사', 'mokpo', '010-555-7777', 7000),
-	(20120005, '류현진', 'm', 22, '컴퓨터공학', 'seoul', '010-122-2222', 8000),
-	(20120006, '손흥민', 'm', 22, '컴퓨터공학', 'seoul', '010-999-9999', 8000);
-/*!40000 ALTER TABLE `student2` ENABLE KEYS */;
-
--- 테이블 jeju.students 구조 내보내기
-CREATE TABLE IF NOT EXISTS `students` (
-  `name` varchar(50) DEFAULT NULL,
-  `kor` int(11) DEFAULT NULL,
-  `eng` int(11) DEFAULT NULL,
-  `math` int(11) DEFAULT NULL,
-  `tot` int(11) DEFAULT NULL,
-  `avg` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 테이블 데이터 jeju.students:~3 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` (`name`, `kor`, `eng`, `math`, `tot`, `avg`) VALUES
-	('대한이', 90, 90, 90, NULL, NULL),
-	('민국이', 80, 80, 81, NULL, NULL),
-	('만세', 100, 100, 100, NULL, NULL);
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
-
--- 프로시저 jeju.student_insert 구조 내보내기
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `student_insert`(
-	IN `name` VARCHAR(50),
-	IN `kor` INT,
-	IN `mat` INT,
-	IN `eng` INT,
-	IN `schoolcode` CHAR(10),
-	OUT `result` INT
-)
-BEGIN
-	DECLARE exit handler for SQLEXCEPTION
-	BEGIN
-	 ROLLBACK;
-	 SET RESULT = -1;
-	END;
-	START TRANSACTION;
-	 INSERT INTO student(name, kor, mat, eng, schoolcode)
-	 VALUE(name, kor, mat, eng, schoolcode);
-	COMMIT;
-	SET RESULT = 0;
-END//
-DELIMITER ;
-
--- 프로시저 jeju.student_select 구조 내보내기
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `student_select`()
-BEGIN
-	SELECT * FROM STUDENT;
-END//
-DELIMITER ;
-
--- 프로시저 jeju.student_sum 구조 내보내기
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `student_sum`()
-BEGIN
-	DROP TABLE IF EXISTS sungjuk_hab;
-	CREATE TABLE sungjuk_hab(korhab float, mathab float, enghab float) as
-		select sum(kor) as korhab, sum(mat) as mathab, sum(eng) as enghab from student;
-	insert into sungjuk_hab(korhab, mathab, enghab)
-		select round(avg(kor),2), round(avg(mat),2), round(avg(eng),2) from student;
-	select * from sungjuk_hab;
-END//
-DELIMITER ;
-
--- 프로시저 jeju.student_updatename 구조 내보내기
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `student_updatename`(
-	IN `oldname` VARCHAR(20),
-	IN `newname` VARCHAR(20),
-	OUT `result` INT
+INSERT INTO userinfo(userID, name, birthYear, addr, mobile1, mobile2, height, mDate)
+VALUES('BBK', '바비킴', 1973, '서울', '010', '00000000', 176, 2013-05-05),
+('EJW', '은지원', 1972, '경북', '011', '88888888', 176, 2014-03-03),
+('JKW', '조관우', 1965, '경기', '018', '99999999', 176, 2010-10-10),
+('JYP', '조용필', 1950, '경기', '011', '44444444', 176, 2009-04-04),
+('KBS', '김범수', 1979, '경남', '011', '22222222', 176, 2012-04-04),
+('KKH', '김경호', 1971, '전남', '019', '33333333', 176, 2007-07-07),
+('LJB', '임재범', 1963, '서울', '016', '66666666', 176, 2009-09-09),
+('LSG', '이승기', 1989, '서울', '011', '11111111', 176, 2008-08-08);
+INSERT INTO userinfo(userID, name, birthYear, addr, height, mDate) VALUES
+('SSK', '성시경', 1979, '서울', 186, 2013-12-12),
+('YJS', '윤종신', 1969, '경남', 170, 2005-05-05);
 
 
+INSERT INTO purchase(userID, prodName, groupName, price, amount)
+VALUES('KBS', '운동화', '전자', 30, 2),
+('KBS', '노트북', '전자', 1000, 1),
+('JYP', '모니터', '전자', 200, 1),
+('BBK', '모니터', '전자', 200, 5),
+('KBS', '청바지', '의류', 50, 3),
+('BBK', '메모리', '전자', 80, 10),
+('SSK', '책', '서적', 15, 5),
+('EJW', '책', '서적', 15, 2),
+('EJW', '청바지', '의류', 50, 1),
+('BBK', '운동화', '전자', 30, 2),
+('EJW', '책', '서적', 15, 1),
+('BBK', '운동화', '서적', 30, 2);
+
+SELECT * FROM purchase;
+SELECT * FROM userinfo;
 
 
-)
-BEGIN
-	DECLARE cnt int default 0;
-	select count(*) into cnt from student where name = oldname;
-	
-	if cnt > 0 then
-		update student set name = newname where name = oldname;
-		set result := 0;
-			
-	else set result := -1;
-	end if;
-END//
-DELIMITER ;
+# BBK가 구매한 데이터를 출력하시요 
+SELECT * FROM purchase WHERE userID like 'BBK';
 
--- 프로시저 jeju.student_updateone 구조 내보내기
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `student_updateone`()
-BEGIN
-	# 모든 데이터에 1점씩 추가 ㅇ_ㅇ!
-	DECLARE _done INT DEFAULT FALSE; # 무한루프의 종료조건
-	DECLARE name_var VARCHAR(20); # 하나의 데이터를 처리하기 위한 변수
-	DECLARE kor_var INT;
-	DECLARE mat_var INT;
-	DECLARE eng_var INT;
-	# cursor : 데이터를 가리키는 위치
-	DECLARE CURSOR_STUDENT CURSOR FOR SELECT name, kor, mat, eng FROM student;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET _done = TRUE;
-	OPEN CURSOR_STUDENT;
-	REPEAT
-		FETCH CURSOR_STUDENT INTO name_var, kor_var, mat_var, eng_var;
-		IF NOT _done THEN # 거짓을 부정 (=참)
-			UPDATE student SET kor = kor_var+1, mat = mat_var+1, eng = eng_var+1 WHERE name = name_var;
-			SET _done = FALSE;
-		END IF;
-	UNTIL _done
-	END REPEAT;
-	CLOSE CURSOR_STUDENT; 
-END//
-DELIMITER ;
+# 김씨인 사람 중 이름과 키를 기준으로 이름, 키 주소를 내림차순으로 출력하시요 ( 이름 뒤에는 '님'을 붙여서 출력)
+SELECT concat(NAME,'님'), height, addr FROM userinfo WHERE NAME LIKE '김%' ORDER BY NAME, height DESC;
 
--- 테이블 jeju.sungjuk_hab 구조 내보내기
-CREATE TABLE IF NOT EXISTS `sungjuk_hab` (
-  `korhab` float DEFAULT NULL,
-  `mathab` float DEFAULT NULL,
-  `enghab` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 jeju.sungjuk_hab:~2 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `sungjuk_hab` DISABLE KEYS */;
-INSERT INTO `sungjuk_hab` (`korhab`, `mathab`, `enghab`) VALUES
-	(733, 705, 704),
-	(91.63, 88.13, 88);
-/*!40000 ALTER TABLE `sungjuk_hab` ENABLE KEYS */;
+# user를 출생년도별 오름차순으로 이름, 주소, 키, 출생년도를 출력하시요( 이름은 성을 빼고 출력) 
+SELECT substr(NAME,2) AS '이름', addr AS '주소', height AS '키', birthYear AS '출생년도'  FROM userinfo ORDER BY birthYear ASC;
 
--- 테이블 jeju.userinfo 구조 내보내기
-CREATE TABLE IF NOT EXISTS `userinfo` (
-  `userID` char(3) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `birthYear` int(11) DEFAULT NULL,
-  `addr` varchar(20) DEFAULT NULL,
-  `mobile1` char(3) DEFAULT NULL,
-  `mobile2` char(8) DEFAULT NULL,
-  `height` int(11) DEFAULT NULL,
-  `mDate` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+# 모바일 번호가 011로 시작하는 사람이 몇명인지 출력하시요 
+SELECT COUNT(*) FROM userinfo WHERE mobile1 LIKE '011';
 
--- 테이블 데이터 jeju.userinfo:~0 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
-INSERT INTO `userinfo` (`userID`, `name`, `birthYear`, `addr`, `mobile1`, `mobile2`, `height`, `mDate`) VALUES
-	('BBK', '바비킴', 1973, '서울', '010', '00000000', 176, '2003'),
-	('EJW', '은지원', 1972, '경북', '011', '88888888', 176, '2008'),
-	('JKW', '조관우', 1965, '경기', '018', '99999999', 176, '1990'),
-	('JYP', '조용필', 1950, '경기', '011', '44444444', 176, '2001'),
-	('KBS', '김범수', 1979, '경남', '011', '22222222', 176, '2004'),
-	('KKH', '김경호', 1971, '전남', '019', '33333333', 176, '1993'),
-	('LJB', '임재범', 1963, '서울', '016', '66666666', 176, '1991'),
-	('LSG', '이승기', 1989, '서울', '011', '11111111', 176, '1992'),
-	('SSK', '성시경', 1979, '서울', NULL, NULL, 186, '1989'),
-	('YJS', '윤종신', 1969, '경남', NULL, NULL, 170, '1995');
-/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
+# 출생년도가 1960년에서 1979년까지 태어난 사람들이 구매한 품목을 출력하시요 
+SELECT pc.prodName FROM purchase pc INNER JOIN userinfo us on (us.birthYear BETWEEN 1960 AND 1979) AND pc.userID = us.userID;
 
--- 트리거 jeju.student_before_insert 구조 내보내기
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `student_before_insert` BEFORE INSERT ON `student` FOR EACH ROW BEGIN -- OLD : 입력된 값, NEW 새롭게 만들어질 데이터
-	SET NEW.total = NEW.kor + NEW.mat + NEW.eng;
-	SET NEW.average = round(NEW.total/3,2);
-	
-	IF NEW.average >= 90 THEN SET NEW.grade='A';
-	ELSEIF NEW.average >= 80 THEN SET NEW.grade='B';
-	ELSEIF NEW.average >= 70 THEN SET NEW.grade='C';
-	ELSEIF NEW.average >= 60 THEN SET NEW.grade='D';
-	ELSE SET NEW.grade='F';
-	END IF;
-END//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
+# UserID별로 구매한 price, amount합계를 출력하시요 ( 이 때 userID는 name으로 출력하시요 )
+SELECT us.name, SUM(pc.price) AS 'price합', SUM(pc.amount) AS 'amount합' 
+FROM purchase pc INNER JOIN userinfo us on pc.userID = us.userID GROUP BY pc.userID;
 
--- 트리거 jeju.student_before_update 구조 내보내기
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `student_before_update` BEFORE UPDATE ON `student` FOR EACH ROW BEGIN
-	SET NEW.total = NEW.kor + NEW.mat + NEW.eng;
-	SET NEW.average = round(NEW.total/3,2);
-	
-	IF NEW.average >= 90 THEN SET NEW.grade='A';
-	ELSEIF NEW.average >= 80 THEN SET NEW.grade='B';
-	ELSEIF NEW.average >= 70 THEN SET NEW.grade='C';
-	ELSEIF NEW.average >= 60 THEN SET NEW.grade='D';
-	ELSE SET NEW.grade='F';
-	END IF;
-END//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
+# 주소가 서울인 사람이 구매한 총액을 구하시요
+SELECT sum(pc.amount*pc.price) AS '구매총액' FROM purchase pc INNER JOIN userinfo us ON us.userID = pc.userID AND us.addr LIKE '서울';
+# 계산 잘 되었나 확인 SELECT pc.amount, pc.price FROM purchase pc INNER JOIN userinfo us ON us.userID = pc.userID AND us.addr LIKE '서울';
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+# 품목별 판매 총액을 출력하시요
+SELECT prodName, SUM(price*amount) AS '판매총액'  FROM purchase GROUP BY prodName;
+
+# 출생년도가 1970년도 이상인 사람을 대상으로 구매한 갯수가 2개 이상인 것의 판매 총합계를 구하시요 
+SELECT SUM(pc.price*pc.amount) AS '판매총합계' FROM purchase pc INNER JOIN userinfo us ON us.userID = pc.userID AND pc.amount >= 2 AND us.birthYear >= 1970;
+
+# 모든 유저를 출력하시요 (이름 중에 김씨와 조씨는 모두 컬씨로 바꾸어 출력)
+SELECT NAME, if (name LIKE '김%', CONCAT('컬',SUBSTR(name,2)), name) AS '바뀐이름' FROM userinfo
+# 바뀐이름만 출력
+SELECT if (name LIKE '김%' OR name LIKE '조%', CONCAT('컬',SUBSTR(name,2)), name) AS '바뀐이름' FROM userinfo
+
+SELECT case when name LIKE '김%' OR name like '조%' then CONCAT('컬', SUBSTR(name,2))
+		ELSE name END name FROM userinfo ;
+
+# 책을 구매한 사람의  이름과 출생년도와 주소를 출력하시요 
+SELECT us.name, us.birthYear, us.addr FROM userinfo us INNER JOIN purchase pc ON us.userID = pc.userID AND prodName LIKE '책'
+GROUP BY us.name;
+# 중복을 제거하기위해서 그룹바이
--- sql3.sql
+++ sql3.sql
@@ -31,3 +31,116 @@ SELECT MIN(money) INTO @minvalue FROM student; SELECT @minvalue;
 SELECT Max(money) - min(money) INTO @rangevalue FROM student; SELECT @rangevalue;
 SELECT (money-@minvalue)/@rangevalue AS 정규화값 FROM student;
 
+INSERT INTO school(schoolname, address, schoolcode, studentcount) VALUES("충주여자고등학교", "충주시", "CH00000001", 300);
+INSERT INTO school(schoolname, address, schoolcode, studentcount) VALUES("신성여자고등학교", "제주시", "IC00000001", 560);
+INSERT INTO school(schoolname, address, schoolcode, studentcount) VALUES("종로여자고등학교", "종로구", "JR00000001", 300);
+INSERT INTO school(schoolname, address, schoolcode, studentcount) VALUES("제주여자고등학교", "제주시", "SE00000001", 300);
+
+INSERT INTO student(NAME, kor, mat, eng, schoolcode) VALUES("전공인", 81, 81, 81, "SE00000001"),
+("전공이", 81, 81, 81, "SE00000001"),
+("전공삼", 81, 81, 81, "CH00000001"),
+("전공사", 91, 100, 100, "CH00000001"),
+("고려인", 100, 100, 100, "CH00000001"),
+("종로구", 100, 81, 81, "IC00000001"),
+("김만덕", 100, 81, 71, "IC00000001");
+
+SELECT * FROM school;
+SELECT * FROM student;
+SELECT * FROM school WHERE schoolcode = 'CH00000001';
+SELECT * from student where NAME='고려인';
+SELECT NAME, kor, eng, mat FROM student WHERE NAME = '전공인';
+SELECT NAME AS '이름', kor AS '국어', mat AS '수학', eng AS '영어' FROM student WHERE NAME = '전공이';
+
+# 서브 쿼리 
+SELECT NAME, kor, mat, eng FROM student WHERE schoolcode
+	IN (SELECT schoolcode FROM school WHERE schoolname = '신성여자고등학교');
+	
+SELECT NAME, kor, mat, eng FROM student WHERE NAME LIKE '전공%';
+SELECT NAME, kor, mat, eng FROM student WHERE NAME LIKE '%공%';
+SELECT NAME, kor, mat, eng FROM student WHERE NAME LIKE '_려_';
+
+SELECT * FROM student WHERE kor>90 AND mat>90 AND eng>90;
+SELECT * FROM student WHERE kor>90 OR mat>90 OR eng>90;
+SELECT * FROM student WHERE kor>80 AND kor<90;
+SELECT * FROM student WHERE kor BETWEEN 80 AND 100;
+
+# group by, having, order by, limit
+SELECT * FROM student LIMIT 1;
+
+SELECT * FROM student ORDER BY NAME DESC; # descending 내림차순
+SELECT * FROM student ORDER BY NAME ASC; # ascending 오름차순
+
+SELECT sc.schoolname, sc.schoolcode, st.name, st.average FROM student st INNER JOIN school sc # 두개의 조건이 만족
+ON st.schoolcode = sc.schoolcode;
+
+# outer >> left / right / (full)
+
+SELECT NAME AS '이름', kor AS '국어', mat AS '수학', eng AS '영어' FROM student WHERE NAME = '전공이';
+SELECT CONCAT(NAME,'님') AS '이름', kor AS '국어', mat AS '수학', eng AS '영어' FROM student WHERE NAME = '전공인';
+SELECT CONCAT(NAME,'님') AS '이름', LPAD(kor, 10, '*') AS '국어', mat AS '수학', eng AS '영어' FROM student WHERE NAME='전공삼';
+SELECT REPLACE(CONCAT(NAME,'님'), '님', '형') AS '이름', LPAD(kor, 10, '*') AS '국어', mat AS '수학', eng AS '영어'
+	FROM student WHERE NAME='전공삼';
+SELECT SUBSTR(REPLACE(CONCAT(NAME,'님'),'님','형'),1,2) AS '이름', LPAD(kor, 10, '*') AS '국어', mat AS '수학', eng AS '영어'
+	FROM student WHERE NAME='전공삼';
+SELECT LENGTH(SUBSTR(REPLACE(CONCAT(NAME, '님'),'님','형'),1,2)) AS '이름', LPAD(kor, 10, '*') AS '국어', mat AS '수학', eng AS '영어'
+	FROM student WHERE NAME = '전공이';
+
+# 문제
+-- 전공인의 국어 : 80, 영어 : 90 으로 변경하고 합계와 평균 그리고 grade를 수정 (계산을 수동으로 하고, 변화만 적용하시오)
+UPDATE student SET kor = 80, eng = 90, total = 215, average = 83.67, grade = 'B' WHERE NAME = '전공인';
+# insert trigger 만 있어서 따로 계산을 해줘야함 (challenge! update trigger)
+UPDATE student SET kor = 80, eng = 90 WHERE NAME = '전공인';
+UPDATE student SET total = 90+80+90, average = ROUND((90+80+90)/3,2), grade = 'B' WHERE NAME = '전공인';
+
+UPDATE student SET total=kor+mat+eng, average = ROUND((kor+mat+eng)/3,2),
+	grade =
+	(case when (average >= 90) then 'A'
+	when (average >= 80) then 'B'
+	when (average >= 70) then 'C'
+	when (average >= 60) then 'D'
+	ELSE 'F'
+	END)
+	WHERE NAME = '전공인';
+
+SELECT * FROM student;
+-- student 중 kor, mat 점수만 출력하시오
+SELECT kor, mat FROM student;
+-- student 중 average 가 90이상인 사람의 name, schoolname 을 출력하시오
+SELECT st.name, sc.schoolname FROM student st inner join school sc on st.average >= 90 and st.schoolcode = sc.schoolcode;
+-- student 중 eng 점수를 출력하되 그 이름을 '영어'로 하시오
+SELECT NAME, eng AS '영어' FROM student;
+-- 이름 가운데 자가 '공'인 사람을 모두 출력하시오
+SELECT * FROM student WHERE NAME LIKE '%공%';
+-- 학교 이름이 충주 여자 고등학교인 사람의 name 과 total을 출력하시오
+SELECT st.name, st.total FROM student st INNER JOIN school sc ON sc.schoolcode = st.schoolcode AND sc.schoolname='충주여자고등학교';
+SELECT st.name, st.total FROM student st INNER JOIN school sc ON sc.schoolname='충주여자고등학교' AND sc.schoolcode = st.schoolcode;
+-- student 에서 total을 이용하여 정렬하고 상위 2사람만 출력하시오
+SELECT * FROM student ORDER BY total DESC LIMIT 2;
+
+# 순서 : where, group by, having, order by, limit
+# join은 from절에 오는거! (위 순서랑 상관없음)
+
+# 저장루틴
+# 저장 프로시져 연습 (서버 측에서 테스트하는 것 call)
+CALL student_select();
+CALL student_insert('제주도', 100, 100, 100, 'IC00000001', @res);
+SELECT @res;
+
+# 문제
+# 이름을 변경하는 프로시져를 작성하시오
+CALL student_name_change('제주도', '김제주', @res);
+SELECT @res;
+CALL student_select();
+
+CALL student_updatename(' ', '팔전공', @res);
+SELECT @res;
+CALL student_select();
+
+CALL student_updatename('전공삼', '팔전공', @res);
+SELECT @res;
+CALL student_select();
+
+CALL student_sum();
+
+CALL student_updateone();
+CALL student_select();
